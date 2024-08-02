ecdac66d15d99682e473e8d1a9a02889c5aa43d4
関口晃毅 <168495408+sekiguchiteruki@users.noreply.github.com>
2024-08-03 02:07:16 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,11 +1,11 @@
 alert('Hello,Teruki!');
 alert(1+1);
-alert(Math.sqrt(1+1));
+alert(Math.sqrt(1+1))
 alert(Math.sqrt(2+3));
 alert(1/0);
 alert(0/0);
 alert(2+3);
-alert('2'+'3');
-alert(Number('2')+NUmber('3'));
-alret('2'+3));
-alret(Number('2')+3);
+alert("2"+"3");
+alert(Number("2")+Number("3"));
+alert("2"+3);
+alert(Number("2")+3);
