82d622e305f79c9ae2f495bc5fb3c9576a3de62b
関口晃毅 <168495408+sekiguchiteruki@users.noreply.github.com>
2024-08-03 01:16:28 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -6,8 +6,8 @@ alert(Math.sqrt(2+3));
 alert(1/0);
 alert(0/0);
 
-alert(Math.sqrt(2+3));
-alert(Math.sqrt("2"+"3"));
-alert(Math.sqrt(Number("2")+("3")));
-alert(Math.sqrt("2"+3));
-alert(Math.sqrt(Number("2")+3));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+NUmber('3'));
+alret('2'+3));
+alret(Number('2')+3);
